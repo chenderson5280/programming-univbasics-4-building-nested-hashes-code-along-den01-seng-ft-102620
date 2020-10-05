dc2cc5991a314a8022d6bdebc21c7f58f79593ce
Christopher Henderson <chenderson5280@gmail.com>
2020-10-05 23:50:17 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,14 +1,14 @@
 def second_challenge
   epic_tragedy = {
-    :montague => { patriarch: "" ,
-                  matriarch: "" ,
-                  hero: "" ,
+    :montague => { patriarch: {} ,
+                  matriarch: {} ,
+                  hero: {} ,
                   hero_friends: []
       #your key/value pairs here!
       },
-    :capulet => { patriarch: "" ,
-                  matriarch: "" ,
-                  heroine: "" ,
+    :capulet => { patriarch: {} ,
+                  matriarch: {} ,
+                  heroine: {} ,
                   heroine_friends: []
       #your key/value pairs here!
       }}
