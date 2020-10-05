89b5939c32926d5a9f2f0079cc7af2229d5c40bf
Christopher Henderson <chenderson5280@gmail.com>
2020-10-05 23:52:49 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => { :name => "",
-                      :age => ""]
+                      :age => ""
         #your key/value pairs here
         },
       :matriarch => {
@@ -15,7 +15,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => { :name => "",
-                      :age => ""]
+                      :age => ""
         #your key/value pairs here
         },
       :matriarch => {
