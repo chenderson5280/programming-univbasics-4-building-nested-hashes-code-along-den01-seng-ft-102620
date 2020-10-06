abcd19d4f538e9b25b83f757c15de20c7fe0646f
Christopher Henderson <chenderson5280@gmail.com>
2020-10-06 00:07:16 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
                 :status => "alive"
         #your key/value pairs here
         },
-      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+      :hero_friends => []
    },
    :capulet => {
       :patriarch => { :name => "Lord Capulet",
